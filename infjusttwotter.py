1a0a9b8c047782447293931281a73790605977f6
cosmicbhejafry <47458458+cosmicbhejafry@users.noreply.github.com>
2021-03-11 17:55:34 +0530

    Update infjusttwotter.py

--- infjusttwotter.py
+++ infjusttwotter.py
@@ -6,10 +6,10 @@ import time
 from datetime import datetime as dt
 from os import environ
 
-consumer_key = environ[consumer_key]
-consumer_secret =  environ[consumer_secret]
-access_token = environ[access_token]
-access_token_secret = environ[access_token_secret]
+consumer_key = environ['consumer_key']
+consumer_secret =  environ['consumer_secret']
+access_token = environ['access_token']
+access_token_secret = environ['access_token_secret']
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)

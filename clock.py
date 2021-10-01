29103b3c691423c2893bec94a50bcd21b938357f
cosmicbhejafry <mt1180754@iitd.ac.in>
2021-10-01 13:50:18 +0530

    update clock fibonacci test

--- clock.py
+++ clock.py
@@ -2,12 +2,32 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=42)
-def timed_job():
-    print('This job is run every 42 minutes.')
+# @sched.scheduled_job('interval', minutes=42)
+# def timed_job():
+#     print('This job is run every 42 minutes.')
 
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-# def scheduled_job():
-#     print('This job is run every weekday at 5pm.')
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=1)
+def scheduled_job():
+    print('This job is run every weekday at 1am')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=3)
+def scheduled_job():
+    print('This job is run every weekday at 3am')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=5)
+def scheduled_job():
+    print('This job is run every weekday at 5am.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=8)
+def scheduled_job():
+    print('This job is run every weekday at 8am.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=13)
+def scheduled_job():
+    print('This job is run every weekday at 13pm.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=21)
+def scheduled_job():
+    print('This job is run every weekday at 21pm.')
 
 sched.start()

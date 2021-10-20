6bfeb44c2739c8898d3b9761b3298229ba7954a2
cosmicbhejafry <47458458+cosmicbhejafry@users.noreply.github.com>
2021-10-21 00:08:13 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -10,24 +10,24 @@ sched = BlockingScheduler()
 def scheduled_job():
     print('This job is run every weekday at 1am')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=3)
-def scheduled_job():
-    print('This job is run every weekday at 3am')
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=3)
+# def scheduled_job():
+#     print('This job is run every weekday at 3am')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=5)
-def scheduled_job():
-    print('This job is run every weekday at 5am.')
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=5)
+# def scheduled_job():
+#     print('This job is run every weekday at 5am.')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=8)
-def scheduled_job():
-    print('This job is run every weekday at 8am.')
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=8)
+# def scheduled_job():
+#     print('This job is run every weekday at 8am.')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=13)
-def scheduled_job():
-    print('This job is run every weekday at 13pm.')
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=13)
+# def scheduled_job():
+#     print('This job is run every weekday at 13pm.')
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=21)
-def scheduled_job():
-    print('This job is run every weekday at 21pm.')
+# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=21)
+# def scheduled_job():
+#     print('This job is run every weekday at 21pm.')
 
 sched.start()
